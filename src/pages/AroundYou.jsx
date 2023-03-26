f225b561f6098b5ae1d6c6b4c08f63aeeebf756d
Rosa Kang <therosessom@gmail.com>
2023-03-26 14:53:45 -0700

    fix bug

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
 
-export default CountryTracks;
+const AroundYou = () => {
+    const [first, setFirst] = useState('');
+    const [Loading, setLoading] = useState(true);
+    const [activeSong, isPlaying] = useSelector((state) => state.player);
+
+    useEffect(() => {
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_FsgCiaxfzkiM2W12I3bHs0NOkVyh2`)
+            .then((res) => setCountry(res?.data?.location?.country))
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }, [country])
+    
+    return (
+        <div></div>
+    )
+}
+
+export default AroundYou;
