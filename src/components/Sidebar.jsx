ec4c4a472aa90fb2e109d4cdc59f1ac03ea07549
Rosa Kang <therosessom@gmail.com>
2023-03-26 19:49:51 -0700

    app final

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,32 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
 import { logo } from '../assets';
-import { links } from '../assets/constants';
-import { HiOutlineMenu } from 'react-icons/hi';
 
-const NavLinks = ({handleClick}) => (
-  <div className='mt-10'>
-    {links.map((link)=> (
-      <NavLink 
-        key={link.name} 
-        to={link.to}
-        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
-        onClick={()=> handleClick && handleClick()}
+const links = [
+  { name: 'Discover', to: '/', icon: HiOutlineHome },
+  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
       >
-        <link.icon className='w-6 h-6 mr-2' />
-        {link.name}
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
       </NavLink>
     ))}
   </div>
-)
-
+);
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -29,7 +34,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <a href="/"><img src={logo} alt="logo" className="w-full h-14 object-contain" /></a>
         <NavLinks />
       </div>
 
@@ -47,7 +52,7 @@ const Sidebar = () => {
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
-  )
+  );
 };
 
 export default Sidebar;
